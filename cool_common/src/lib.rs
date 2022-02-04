3dda79d4c8ea7e78f4c87e20de119ab21bd0cce5
Alexander Raish <leidenn2509@gmail.com>
2022-02-04 07:46:20 +0700

    backup

--- cool_common/src/lib.rs
+++ cool_common/src/lib.rs
@@ -1,4 +1,5 @@
 use std::fmt::{Debug, Formatter};
+use serde::Serialize;
 
 #[derive(Debug, Clone)]
 pub enum Token {
@@ -55,6 +56,15 @@ pub struct Lexeme(pub usize, pub Token, pub String);
 
 impl Debug for Lexeme {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "<{}, {:?}, {:?}>", self.0, self.1, self.2)
+        // write!(f, "<{}, {:?}, {:?}>", self.0, self.1, self.2)
+        write!(f, "#{}, {:?}, {:?}>", self.0, self.1, self.2)
     }
+}
+
+pub struct LexemeSerializer {
+    output: String
+}
+
+pub fn to_string(value: &Lexeme) -> Result<String> {
+
 }
